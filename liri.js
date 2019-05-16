4ae9bf9ca2bb1900f76276e0731bb665fab1addb
Trash Panda <d.george.schlosser@gmail.com>
2019-05-15 23:14:51 -0400

    'ReferenceError: Spotify is not defined' FIXED by installing node-spotify-api

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 // code to read and set any environment variables with the dotenv package
 require("dotenv").config();
+// code to read and set any environment variables with the node-spotify-api package
+var Spotify = require('node-spotify-api');
 // code required to import the keys.js file and store it in a variable
 var keys = require("./keys.js");
 // access your keys information
